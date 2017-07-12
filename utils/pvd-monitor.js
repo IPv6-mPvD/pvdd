e72ef154d708773aa8dd33965631aa408ef1f199
Thierry Danis <tdanis@cisco.com>
2017-07-12 13:08:11 +0200

    pvd-monitor.js : rewritten to use the pvdd node package. Better start it via the pvd-monitor script

--- utils/pvd-monitor.js
+++ utils/pvd-monitor.js
@@ -33,6 +33,8 @@ var schedule = require("node-schedule");
 var http = require("http");
 var https = require("https");
 
+var pvdd = require("pvdd");
+
 var Verbose = false;
 var DevelopmentEnvironment = false;
 var DefaultPvd = [];
@@ -52,7 +54,8 @@ function tlog(s) {
 	}
 }
 
-function ComplainConnection(Port, msg, err) {
+function ComplainConnection(msg, err) {
+	var Port = parseInt(process.env["PVDID_PORT"]) || 10101;
 	console.log(msg + "@pvdid-daemon:" + Port + ": " + err.code);
 }
 
@@ -132,49 +135,83 @@ function GetJson(s) {
  * Control connection related functions. The control connection will be used to
  * send attributes updates
  */
-function ControlSockInit(sock) {
-	sock.write("PVDID_CONNECTION_PROMOTE_CONTROL\n");
-	DefaultPvd.forEach(function(pvdId) {
-		sock.write("PVDID_CREATE_PVDID 0 " + pvdId + "\n");
-	});
-}
+var controlCnx = pvdd.connect({
+	autoReconnect : true, 
+	controlConnection : true
+});
 
-var controlSock = null;
-var controlSucceeded = true;
+controlCnx.succeeded = true;
 
-function controlConnection(Port) {
-	/*
-	 * Perform the initial connection, and automatic reconnection
-	 * in case we lose connection with it
-	 */
-	if (controlSock == null) {
-		controlSock = Net.connect({ host : "0.0.0.0", port : Port });
-		controlSock.on("connect", function() {
-			controlSucceeded = true;
-			ControlSockInit(controlSock);
-			console.log("Control connection established with pvdid-daemon");
-		});
-		controlSock.on("error", function(err) {
-			if (controlSucceeded) {
-				ComplainConnection(Port, "control socket", err);
-			}
-			controlSucceeded = false;
-			controlSock = null;
-		});
+controlCnx.on("connect", function() {
+	console.log("Control connection established with pvdd");
+	controlCnx.succeeded = true;
+	DefaultPvd.forEach(controlCnx.createPvd);
+});
+controlCnx.on("error", function(err) {
+	if (controlCnx.succeeded) {
+		ComplainConnection("control socket", err);
 	}
-	else {
-		controlSock.write("\n");	// ping : may trigger an error
-	}
-	setTimeout(controlConnection, 1000, Port);
-}
+	controlCnx.succeeded = false;
+});
+
 
 /*
- * MonitorPvD : given a pvdId, check if it is needed to start/stop retrieving
- * its associated JSON extra info (that's what we call monitoring a PvD)
+ * Regular connection related functions. The regular connection will be used
+ * to send queries (PvD list and attributes) and to receive
+ * replies/notifications
  */
-function MonitorPvD(sock, pvdId, forceNow) {
+var regularCnx = pvdd.connect({ autoReconnect : true });
+
+regularCnx.succeeded = true;
+
+regularCnx.on("connect", function() {
+	regularCnx.succeeded = true;
+	regularCnx.getList();
+	regularCnx.subscribeNotifications();
+	regularCnx.subscribeAttribute("*");
+	console.log("Regular connection established with pvdd");
+});
+
+regularCnx.on("error", function(err) {
+	if (regularCnx.succeeded) {
+		DelAllPvD();
+		ComplainConnection("regular socket", err);
+	}
+	regularCnx.succeeded = false;
+});
+
+regularCnx.on("pvdList", function(pvdList) {
+	pvdList.forEach(function(pvdId) {
+		if (allPvd[pvdId] == null) {
+			/*
+			 * New PvD => retrieve its attributes
+			 */
+			NewPvD(pvdId);
+			regularCnx.getAttributes(pvdId);
+		}
+	});
+	dlog("New pvd list : " + JSON.stringify(allPvd, null, 4));
+});
+
+regularCnx.on("delPvd", DelPvD);
+
+regularCnx.on("pvdAttributes", function(pvdId, attrs) {
+	/*
+	 * update the internal attributes structure and restart the PvD
+	 * monitoring
+	 */
+	dlog("Attributes for " + pvdId + " = " + JSON.stringify(attrs, null, 8));
+
 	if (allPvd[pvdId] != null) {
-		if (allPvd[pvdId].attributes.hFlag) {
+		/*
+		 * Check the Seq field for changes
+		 */
+		var forceNow =
+			attrs.sequenceNumber != allPvd[pvdId].attributes.sequenceNumber;
+
+		allPvd[pvdId].attributes = attrs;
+
+		if (attrs.hFlag) {
 			if (forceNow || ! allPvd[pvdId].monitored) {
 				allPvd[pvdId].monitored = true;
 				RetrievePvdExtraInfo(pvdId);
@@ -194,208 +231,11 @@ function MonitorPvD(sock, pvdId, forceNow) {
 			/*
 			 * In any case, we must delete the extraInfo field
 			 */
-			UnsetAttribute(pvdId, "extraInfo");
+			controlCnx.unsetAttribute(pvdId, "extraInfo");
 		}
 	}
-}
+});
 
-/*
- * UpdateAttributes : update the internal attributes structure for a given
- * pvdId and restart the PvD monitoring. This function is called when
- * the attributes for the PvD have been received
- */
-function UpdateAttribute(sock, pvdId, attributes) {
-	dlog("UpdateAttribute : pvdId = " + pvdId + ", attributes = " + attributes);
-	if (allPvd[pvdId] != null) {
-		var J;
-		if ((J = GetJson(attributes)) != null) {
-			/*
-			 * Check the Seq field for changes
-			 */
-			var forceNow =
-				J.sequenceNumber != allPvd[pvdId].attributes.sequenceNumber;
-			allPvd[pvdId].attributes = J;
-			MonitorPvD(sock, pvdId, forceNow);
-			dlog("Attribute for " + pvdId + 
-				" = " + JSON.stringify(J, null, 8));
-		}
-	}
-}
-
-/*
- * HandleMultiLine : a multi-line message has been fully read
- * Parses the message and handles it
- */
-function HandleMultiLine(sock, msg) {
-	var r;
-
-	dlog("HandleMultiLine : msg = " + msg);
-
-	if ((r = msg.match(/PVDID_ATTRIBUTES +([^ \n]+)\n([\s\S]+)/i)) != null) {
-		UpdateAttribute(sock, r[1], r[2]);
-		return;
-	}
-	return;
-}
-
-/*
- * HandleOneLine : one line message handling. The trailing
- * \n must have been removed
- */
-var multiLines = false;
-var fullMsg = "";
-
-function HandleOneLine(sock, msg) {
-	var r;
-
-	dlog("Handling one line : " + msg + " (multiLines = " + multiLines + ")");
-
-	/*
-	 * We check the beginning of a multi-lines section
-	 * before anything else, to reset the buffer in case
-	 * a previous multi-lines was improperly closed
-	 */
-	if (msg == "PVDID_BEGIN_MULTILINE") {
-		multiLines = true;
-		fullMsg = "";
-		return;
-	}
-
-	/*
-	 * End of a multi-lines section ?
-	 */
-	if (msg == "PVDID_END_MULTILINE") {
-		HandleMultiLine(sock, fullMsg);
-		multiLines  = false;
-		return;
-	}
-
-	/*
-	 * Are we in a mult-line section ?
-	 */
-	if (multiLines) {
-		fullMsg += msg + "\n";
-		return;
-	}
-
-	/*
-	 * Single line messages
-	 */
-	if ((r = msg.match(/PVDID_LIST +(.*)/i)) != null) {
-		if ((newListPvD = r[1].match(/[^ ]+/g)) == null) {
-			return;
-		}
-		newListPvD.forEach(function(pvdId) {
-			if (allPvd[pvdId] == null) {
-				/*
-				 * New PvD => retrieve its attributes
-				 */
-				NewPvD(pvdId);
-				sock.write("PVDID_GET_ATTRIBUTES " + pvdId + "\n");
-			}
-		});
-		dlog("New pvd list : " + JSON.stringify(allPvd, null, 4));
-		return;
-	}
-
-	if (msg.match(/PVDID_NEW_PVDID.*/i) != null) {
-		/*
-		 * Ignore them (we prefer using PVDID_LIST instead)
-		 */
-		return;
-	}
-
-	if ((r = msg.match(/PVDID_DEL_PVDID +([^ ]+)/i)) != null) {
-		/*
-		 * We must stop monitoring this PvD and unregister it
-		 */
-		var pvdId = r[1];
-
-		DelPvD(pvdId);
-		return;
-	}
-
-	if ((r = msg.match(/PVDID_ATTRIBUTES +([^ ]+) +(.+)/i)) != null) {
-		UpdateAttribute(sock, r[1], r[2]);
-		return;
-	}
-	return;
-}
-
-/*
- * Regular connection related functions. The regular connection will be used
- * to send queries (PvD list and attributes) and to receive
- * replies/notifications
- */
-function regularSockInit(sock) {
-	sock.write("PVDID_GET_LIST\n");
-	sock.write("PVDID_SUBSCRIBE_NOTIFICATIONS\n");
-	sock.write("PVDID_SUBSCRIBE *\n");
-}
-
-var regularSock = null;
-var regularSucceeded = true;
-
-function regularConnection(Port) {
-	/*
-	 * Perform the initial connection, and automatic reconnection
-	 * in case we lose connection with it
-	 */
-	if (regularSock == null) {
-		regularSock = Net.connect({ host : "0.0.0.0", port : Port });
-		regularSock.on("connect", function() {
-			regularSucceeded = true;
-			regularSockInit(regularSock);
-			console.log("Regular connection established with pvdid-daemon");
-		});
-		regularSock.on("error", function(err) {
-			if (regularSucceeded) {
-				DelAllPvD();
-				ComplainConnection(Port, "regular socket", err);
-			}
-			regularSucceeded = false;
-			regularSock = null;
-		});
-		regularSock.on("data", function(d) {
-			d.toString().split("\n").forEach(function(oneLine) {
-				HandleOneLine(regularSock, oneLine);
-			});
-		});
-	}
-	else {
-		regularSock.write("\n");	// to trigger a connection error
-	}
-	setTimeout(regularConnection, 1000, Port);
-}
-
-/*
- * SetAttribute : send a set attribute query to the daemon
- * The attrValue must be a valid JSON object (actually its
- * string representation)
- */
-function SetAttribute(pvdId, attrName, attrValue) {
-	if (controlSock != null) {
-		dlog("SetAttribute : attrName = " + attrName + ", attrValue = " + attrValue);
-		controlSock.write(
-			"PVDID_BEGIN_TRANSACTION " + pvdId + "\n" +
-			"PVDID_BEGIN_MULTILINE\n" +
-			"PVDID_SET_ATTRIBUTE " + pvdId + " " +
-						attrName + "\n" +
-						attrValue + "\n" +
-			"PVDID_END_MULTILINE\n" +
-			"PVDID_END_TRANSACTION " + pvdId + "\n");
-	}
-}
-
-/*
- * UnsetAttribute : send a unset attribute query to the daemon
- */
-function UnsetAttribute(pvdId, attrName) {
-	if (controlSock != null) {
-		controlSock.write(
-			"PVDID_UNSET_ATTRIBUTE " + pvdId + " " + attrName + "\n");
-	}
-}
 
 function ScheduleRetry(pvdId) {
 	if (allPvd[pvdId] != null) {
@@ -420,7 +260,6 @@ function ScheduleRetry(pvdId) {
 function ScheduleAt(pvdId, DateStr, f, args) {
 	tlog("ScheduleAt at " + DateStr + " programmed for " + pvdId);
 	var j = new schedule.scheduleJob(new Date(DateStr), function() { f(args); });
-	tlog("ScheduleAt : jobs = " + JSON.stringify(j));
 	if (j.name == null) {
 		ScheduleRetry(pvdId);
 		return(null);
@@ -429,19 +268,17 @@ function ScheduleAt(pvdId, DateStr, f, args) {
 }
 
 function RetrievePvdExtraInfo(pvdId) {
-	var Url, protocol;
-
 	if (allPvd[pvdId] == null) {
 		return;
 	}
 
 	if (DevelopmentEnvironment) {
-		Url = "http://localhost:8000/" + pvdId;
-		protocol = http;
+		var Url = "http://localhost:8000/" + pvdId;
+		var protocol = http;
 	}
 	else {
-		Url = "https://" + pvdId + "/pvd.json";
-		protocol = https;
+		var Url = "https://" + pvdId + "/pvd.json";
+		var protocol = https;
 	}
 
 	tlog("Retrieving url " + Url);
@@ -470,8 +307,8 @@ function RetrievePvdExtraInfo(pvdId) {
 				 * the stringified conversion of the JSON object
 				 * to send something cleaner to the daemon
 				 */
-				SetAttribute(pvdId, "extraInfo", JSON.stringify(J, null, 12));
-				tlog("PvdId : " + pvdId + " expires = " + J.expires);
+				controlCnx.setAttribute(pvdId, "extraInfo", J);
+				tlog("[" + pvdId + "] expires = " + J.expires);
 
 				/*
 				 * Schedule if needed the retrieval of the JSON based
@@ -536,10 +373,5 @@ if (Help) {
 	console.log("For this to work, a local http server must be started of course");
 	process.exit(0);
 }
-
-var Port = parseInt(process.env["PVDID_PORT"]) || 10101;
-
-controlConnection(Port);
-regularConnection(Port);
 
 /* ex: set ts=8 noexpandtab wrap: */
