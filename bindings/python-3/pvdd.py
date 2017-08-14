df1a40a2b79fd8322d50e35ededce85bf8e78d06
Thierry Danis <tdanis@cisco.com>
2017-08-14 09:56:14 +0200

    pvdd.py : fix incorrect locking in sock write error. Replace class function calls by instance function calls

--- bindings/python-3/pvdd.py
+++ bindings/python-3/pvdd.py
@@ -110,7 +110,7 @@ pvddCnx.connect(autoReconnect = True)
         if len(r) == 4:
             attr = noerr(json.loads, r[2])
             if attr != None:
-                pvdd.emit(self, "pvdAttributes", r[1], attr)
+                self.emit("pvdAttributes", r[1], attr)
                 if self.verbose:
                     print("Attributes for ", r[1], " = ", r[2])
             return
@@ -119,8 +119,8 @@ pvddCnx.connect(autoReconnect = True)
         if len(r) == 5:
             attr = noerr(json.loads, r[3])
             if attr != None:
-                pvdd.emit(self, "pvdAttribute", r[1], r[2], attr)
-                pvdd.emit(self, "on" + r[2], r[1], attr)
+                self.emit("pvdAttribute", r[1], r[2], attr)
+                self.emit("on" + r[2], r[1], attr)
 
     def handleOneLine(self, msg):
         # We check the beginning of a multi-lines section
@@ -134,7 +134,7 @@ pvddCnx.connect(autoReconnect = True)
         # End of a multi-lines section ?
         if msg == "PVD_END_MULTILINE":
                 if self.fullMsg != None:
-                        pvdd.handleMultiLine(self, self.fullMsg)
+                        self.handleMultiLine(self.fullMsg)
                 self.multiLines  = False
                 return
 
@@ -156,24 +156,24 @@ pvddCnx.connect(autoReconnect = True)
             if self.verbose:
                 print("pvd list =", newListPvD)
 
-            pvdd.emit(self, "pvdList", newListPvD)
+            self.emit("pvdList", newListPvD)
             return
 
         r = re.split("PVD_NEW_PVD +([^ ]+)", msg)
         if len(r) == 3:
-            pvdd.emit(self, "newPvd", r[1])
+            self.emit("newPvd", r[1])
             return
 
         r = re.split("PVD_DEL_PVD +([^ ]+)", msg)
         if len(r) == 3:
-            pvdd.emit(self, "delPvd", r[1])
+            self.emit("delPvd", r[1])
             return
 
         r = re.split("PVD_ATTRIBUTES +([^ ]+) +(.+)", msg)
         if len(r) == 4:
             attr = noerr(json.loads, r[2])
             if attr != None:
-                pvdd.emit(self, "pvdAttributes", r[1], attr)
+                self.emit("pvdAttributes", r[1], attr)
                 if self.verbose:
                     print("Attributes for ", r[1], " = ", r[2])
             return
@@ -182,8 +182,8 @@ pvddCnx.connect(autoReconnect = True)
         if len(r) == 5:
             attr = noerr(json.loads, r[3])
             if attr != None:
-                pvdd.emit(self, "pvdAttribute", r[1], r[2], attr)
-                pvdd.emit(self, "on" + r[2], r[1], attr)
+                self.emit("pvdAttribute", r[1], r[2], attr)
+                self.emit("on" + r[2], r[1], attr)
             return
 
         return
@@ -199,32 +199,32 @@ pvddCnx.connect(autoReconnect = True)
         while self.sock != None:
                 data = self.sock.recv(4096)
                 if data == b'':
-                    pvdd.emit(self, "error", "connection broken")
+                    self.emit("error", "connection broken")
                     if self.verbose:
                         print("Disconnected from pvdd")
-                    pvdd.sockclose(self)
+                    self.sockclose()
                 else:
                     data = data.decode('utf8', errors='strict')
                     for line in data.split("\n"):
                         if line != '':
-                            pvdd.emit(self, "data", line)
-                            pvdd.handleOneLine(self, line)
+                            self.emit("data", line)
+                            self.handleOneLine(line)
 
     def sockwrite(self, msg):
         self.m.acquire()
         if self.sock != None:
             if self.sock.send(bytearray(msg, "utf-8")) == 0:
-                pvdd.emit(self, "error", "connection broken")
+                self.emit("error", "connection broken")
                 if self.verbose:
                     print("Disconnected from pvdd")
-                pvdd.sockclose(True)
+                self.sockclose(doLock = False)
         self.m.release()
 
     def createPvd(self, pvdName):
-        pvdd.sockwrite(self, "PVD_CREATE_PVD 0 " + pvdName + "\n")
+        self.sockwrite("PVD_CREATE_PVD 0 " + pvdName + "\n")
 
     def setAttribute(self, pvdName, attrName, attrValue):
-            pvdd.sockwrite(self, "PVD_BEGIN_TRANSACTION " + pvdName + "\n" +
+            self.sockwrite("PVD_BEGIN_TRANSACTION " + pvdName + "\n" +
                   "PVD_BEGIN_MULTILINE\n" +
                   "PVD_SET_ATTRIBUTE " + pvdName + " " +
                                     attrName + "\n" +
@@ -233,22 +233,22 @@ pvddCnx.connect(autoReconnect = True)
                   "PVD_END_TRANSACTION " + pvdName + "\n")
 
     def unsetAttribute(self, pvdName, attrName):
-        pvdd.sockwrite(self, "PVD_UNSET_ATTRIBUTE " + pvdName + " " + attrName + "\n")
+        self.sockwrite("PVD_UNSET_ATTRIBUTE " + pvdName + " " + attrName + "\n")
 
     def getList(self):
-        pvdd.sockwrite(self, "PVD_GET_LIST\n")
+        self.sockwrite("PVD_GET_LIST\n")
 
     def getAttributes(self, pvdName):
-        pvdd.sockwrite(self, "PVD_GET_ATTRIBUTES " + pvdName + "\n")
+        self.sockwrite("PVD_GET_ATTRIBUTES " + pvdName + "\n")
 
     def getAttribute(self, pvdName, attrName):
-        pvdd.sockwrite(self, "PVD_GET_ATTRIBUTE " + pvdName + " " + attrName + "\n")
+        self.sockwrite("PVD_GET_ATTRIBUTE " + pvdName + " " + attrName + "\n")
 
     def subscribeAttribute(self, attrName):
-        pvdd.sockwrite(self, "PVD_SUBSCRIBE " + attrName + "\n")
+        self.sockwrite("PVD_SUBSCRIBE " + attrName + "\n")
 
     def subscribeNotifications(self):
-        pvdd.sockwrite(self, "PVD_SUBSCRIBE_NOTIFICATIONS\n")
+        self.sockwrite("PVD_SUBSCRIBE_NOTIFICATIONS\n")
 
     # internalConnection performs the connection with the pvdd daemon
     # It also attempt to reconnect every second in autoReconnect mode
@@ -259,16 +259,16 @@ pvddCnx.connect(autoReconnect = True)
                 self.sock.connect(("localhost", self.port))
                 self.start()    # background run() execution
                 if self.controlConnection:
-                    pvdd.sockwrite(self, "PVD_CONNECTION_PROMOTE_CONTROL\n")
-                pvdd.emit(self, "connect")
+                    self.sockwrite("PVD_CONNECTION_PROMOTE_CONTROL\n")
+                self.emit("connect")
                 if self.verbose:
                     print("Connected with pvdd")
             except Exception as e:
-                pvdd.sockclose(self)
-                pvdd.emit(self, "error", e)
+                self.sockclose()
+                self.emit("error", e)
 
         if self.sock == None and self.autoReconnect:
-            self.timer = threading.Timer(1.0, pvdd.internalConnection, (self,))
+            self.timer = threading.Timer(1.0, self.internalConnection)
             self.timer.start()
 
     def connect(
@@ -284,7 +284,7 @@ pvddCnx.connect(autoReconnect = True)
             self.controlConnection = controlConnection
             self.verbose = verbose
 
-            pvdd.internalConnection(self)
+            self.internalConnection()
 
     def sockclose(self, doLock = True):
         if doLock:
@@ -297,7 +297,7 @@ pvddCnx.connect(autoReconnect = True)
             self.m.release()
 
     def disconnect(self):
-        pvdd.sockclose(self, True)
+        self.sockclose()
         if self.timer != None:
             self.timer.cancel()
             self.timer = None
