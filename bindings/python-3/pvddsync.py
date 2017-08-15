4caa0299fc6e942ada299bb786d1182ef52ada62
Thierry Danis <tdanis@cisco.com>
2017-08-15 15:12:27 +0200

    pvddsync.py ; rename file, completely rewrote synchronization between handler and the main thread (using condvar now instead of queues)

--- bindings/python-3/pvddsync.py
+++ bindings/python-3/pvddsync.py
@@ -18,74 +18,109 @@
 
 import pvdd
 import queue
+import threading
 
 class pvddsync(pvdd.pvdd):
+    def _delete(self):
+        print("_delete called")
+
     def __init__(self, TO = None):
         super().__init__()
 
         # We also create a hidden pvdd connection for synchronous
         # requests/replies pattern
         self.syncPvd = pvdd.pvdd()
-        self.myqueue = queue.Queue()
+        self.syncPvd.on("pvdList", self._handlePvdList)
+        self.syncPvd.on("pvdAttributes", self._handlePvdAttributes)
+        self.syncPvd.on("pvdAttribute", self._handlePvdAttribute)
+        self.condPvdList = threading.Condition()
+        self.condAttributes = threading.Condition()
+        self.condAttribute = threading.Condition()
         self.TO = TO
 
-    def syncResult(self, *args):
-        self.myqueue.put(args)
+    def _handlePvdList(self, syncPvdd, pvdList):
+        with self.condPvdList:
+            self.pvdList = pvdList
+            self.condPvdList.notify()
+
+    def _handlePvdAttributes(self, syncPvdd, pvd, attributes):
+        with self.condAttributes:
+            self.pvdAttributes = (pvd, attributes)
+            self.condAttributes.notify()
+
+    def _handlePvdAttribute(self, syncPvdd, pvd, attrName, attrValue):
+        with self.condAttribute:
+            self.pvdAttribute = (pvd, attrName, attrValue)
+            self.condAttribute.notify()
 
     def connect(self, **args):
         super().connect(**args)
-        self.syncPvd.on("pvdList", self.syncResult)
-        self.syncPvd.on("pvdAttributes", self.syncResult)
-        self.syncPvd.on("pvdAttribute", self.syncResult)
         self.syncPvd.connect(**args)
 
     def disconnect(self):
         super().disconnect()
         self.syncPvd.disconnect()
 
+    def leave(self):
+        super().leave()
+        self.syncPvd.leave()
+
     def getTO(self, TO):
         return self.TO if TO == None else TO
-    
-    def getQueue(self, TO):
-        try:
-            return self.myqueue.get(True, TO)
-        except Exception:
-            return None
 
     def getSyncList(self, TO = None):
-        self.syncPvd.getList()
-        pvdList = self.getQueue(self.getTO(TO))
-        if pvdList != None:
-            _, pvdList = pvdList
-        return pvdList
+        with self.condPvdList:
+            self.syncPvd.getList()
+            self.pvdList = None
+            self.condPvdList.wait(self.getTO(TO))
+
+        return self.pvdList
 
     def getSyncAttributes(self, pvd, TO = None):
-        self.syncPvd.getAttributes(pvd)
-        pvdAttributes = self.getQueue(self.getTO(TO))
-        if pvdAttributes != None:
-            _, pvdname, pvdAttributes = pvdAttributes
-        return pvdAttributes
+        with self.condAttributes:
+            self.syncPvd.getAttributes(pvd)
+            while True:
+                rcvdAttrs = None
+                self.pvdAttributes = None
+                self.condAttributes.wait(self.getTO(TO))
+                if self.pvdAttributes == None:  # timeout
+                    break
+                rcvdPvd, rcvdAttrs = self.pvdAttributes
+                if rcvdPvd == pvd:
+                    break
+
+        return rcvdAttrs
 
     def getSyncAttribute(self, pvd, attrName, TO = None):
-        self.syncPvd.getAttribute(pvd, attrName)
-        attrValue = self.getQueue(self.getTO(TO))
-        if attrValue != None:
-            _, pvdname, attrName, attrValue = attrValue
-        return attrValue
+        with self.condAttribute:
+            self.syncPvd.getAttribute(pvd, attrName)
+            while True:
+                rcvdAttr = None
+                self.pvdAttribute = None
+                self.condAttribute.wait(self.getTO(TO))
+                if self.pvdAttribute == None:   # timeout
+                    break
+                rcvdPvd, rcvdAttrName, rcvdAttr = self.pvdAttribute
+                if rcvdPvd == pvd and rcvdAttrName == attrName:
+                    break
+
+        return rcvdAttr
 
 if __name__ == "__main__":
     def printArgs(pvdd, args):
-        print(args)
+        print("Asynchonous call :", args)
 
     pvddCnx = pvddsync(TO = 0.05)
-    pvddCnx.connect(autoReconnect = True)
+    pvddCnx.connect(autoReconnect = True, verbose = False)
     pvddCnx.on("pvdList", printArgs)
     pvdList = pvddCnx.getSyncList(TO = 0.1)
     print("List of pvds : ", pvdList)
-    pvddCnx.getList()
+    pvddCnx.getList()   # asynchonous call
     if pvdList != None:
         for pvd in pvdList:
             print("Attributes for", pvd, ":", pvddCnx.getSyncAttributes(pvd))
-            print("Addresses for", pvd, ":", pvddCnx.getSyncAttribute(pvd, "addresses"))
+            print("Addresses for", pvd, ":", pvddCnx.getSyncAttribute(pvd, "addresses", TO = 0.1))
 
-    pvddCnx.disconnect()
+    print("Attribute for foo.bar.com :", pvddCnx.getSyncAttributes("foo.bar.com", TO = 0.1))
+
+    pvddCnx.leave()
