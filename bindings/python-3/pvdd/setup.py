2402c8e091d5ebc7586373a59baff703d8dd0476
Thierry Danis <tdanis@cisco.com>
2017-08-17 19:03:16 +0200

    bindings/python3 : preliminary work to package and install the bindings properly

---
+++ bindings/python-3/pvdd/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name='pvdd',
+    version='1.0.0',
+    author='Thierry',
+    author_email='tdanis@cisco.com',
+    packages=['.'],
+    url='https://github.com/IPv6/mPvD/pvdd.git',
+    description='pvdd client bindings for Python3',
+    long_description=open('README').read()
+)
---
+++ bindings/python-3/pvddsync/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name='pvddsync',
+    version='1.0.0',
+    author='Thierry',
+    author_email='tdanis@cisco.com',
+    packages=['.'],
+    url='https://github.com/IPv6/mPvD/pvdd.git',
+    description='pvdd client bindings for Python3 (synchronous extensions)',
+    long_description=open('../README.md').read()
+)
